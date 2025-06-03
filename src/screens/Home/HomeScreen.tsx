c074daf4973e7e09b4d595b67d40c54f24679f6d
Nitcharat Thatthiam <pondaeroz008y@gmail.com>
2025-06-04 01:36:53 +0700

    Remove useLocation in HomeScreen

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useLocation, useNavigate, useOutletContext, } from "react-router-dom";
+import { Link, useNavigate, useOutletContext, } from "react-router-dom";
 import {
   Box,
   Typography,
